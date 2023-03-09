8b7e8727043f06b063033fe285c5bdbf8efbfe71
mariacasanova2346@gmail.com <mariacasanova2346@gmail.com>
2023-03-09 18:47:18 -0500

    AñadirSeccionPaginaPrincipal_MariaCasanova

--- TiendaRopa/src/Vista/PaginaPrincipal.java
+++ TiendaRopa/src/Vista/PaginaPrincipal.java
@@ -28,16 +28,21 @@ public class PaginaPrincipal extends javax.swing.JFrame {
 
         jMenuItem1 = new javax.swing.JMenuItem();
         panel1 = new java.awt.Panel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         panel2 = new java.awt.Panel();
         jLabel4 = new javax.swing.JLabel();
         panel3 = new java.awt.Panel();
-        panel6 = new java.awt.Panel();
+        jLabel6 = new javax.swing.JLabel();
         panel4 = new java.awt.Panel();
+        jLabel10 = new javax.swing.JLabel();
+        panel7 = new java.awt.Panel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
         panel5 = new java.awt.Panel();
-        jLabel5 = new javax.swing.JLabel();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        panel6 = new java.awt.Panel();
+        jLabel2 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
@@ -52,76 +57,79 @@ public class PaginaPrincipal extends javax.swing.JFrame {
         panel1.setBackground(new java.awt.Color(249, 240, 227));
         panel1.setLayout(new java.awt.BorderLayout());
 
-        jLabel1.setText("jLabel1");
-        panel1.add(jLabel1, java.awt.BorderLayout.WEST);
-
-        jLabel2.setText("jLabel2");
-        panel1.add(jLabel2, java.awt.BorderLayout.EAST);
-
-        jLabel3.setText("jLabel3");
-        panel1.add(jLabel3, java.awt.BorderLayout.SOUTH);
-
         panel2.setBackground(new java.awt.Color(249, 240, 227));
         panel2.setLayout(new java.awt.GridLayout(0, 1));
 
         jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vista/3707eb766f47eec3102af0956f8da0c7 (1).png"))); // NOI18N
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Archivos/Logo.png"))); // NOI18N
         panel2.add(jLabel4);
 
-        javax.swing.GroupLayout panel3Layout = new javax.swing.GroupLayout(panel3);
-        panel3.setLayout(panel3Layout);
-        panel3Layout.setHorizontalGroup(
-            panel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 458, Short.MAX_VALUE)
-        );
-        panel3Layout.setVerticalGroup(
-            panel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 56, Short.MAX_VALUE)
-        );
+        panel3.setBackground(new java.awt.Color(163, 144, 129));
+        panel3.setLayout(new java.awt.GridLayout(1, 0));
+
+        jLabel6.setBackground(new java.awt.Color(41, 37, 34));
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vista/ColeccionOtoño2.png"))); // NOI18N
+        panel3.add(jLabel6);
 
         panel2.add(panel3);
 
-        javax.swing.GroupLayout panel6Layout = new javax.swing.GroupLayout(panel6);
-        panel6.setLayout(panel6Layout);
-        panel6Layout.setHorizontalGroup(
-            panel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 456, Short.MAX_VALUE)
-        );
-        panel6Layout.setVerticalGroup(
-            panel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 56, Short.MAX_VALUE)
-        );
+        panel4.setLayout(new java.awt.BorderLayout());
 
-        panel2.add(panel6);
+        jLabel10.setFont(new java.awt.Font("Poppins", 0, 24)); // NOI18N
+        jLabel10.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel10.setText("CONOCE NUESTRAS SECCIONES");
+        panel4.add(jLabel10, java.awt.BorderLayout.NORTH);
 
-        javax.swing.GroupLayout panel4Layout = new javax.swing.GroupLayout(panel4);
-        panel4.setLayout(panel4Layout);
-        panel4Layout.setHorizontalGroup(
-            panel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 458, Short.MAX_VALUE)
-        );
-        panel4Layout.setVerticalGroup(
-            panel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 56, Short.MAX_VALUE)
-        );
+        panel7.setLayout(new java.awt.GridLayout(1, 0));
+
+        jButton1.setBackground(new java.awt.Color(163, 144, 129));
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Archivos/Mujer.png"))); // NOI18N
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        panel7.add(jButton1);
+
+        jButton2.setBackground(new java.awt.Color(163, 144, 129));
+        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Archivos/HOMBRES2.png"))); // NOI18N
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        panel7.add(jButton2);
+
+        jButton3.setBackground(new java.awt.Color(163, 144, 129));
+        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Archivos/BOLSOS1.png"))); // NOI18N
+        panel7.add(jButton3);
+
+        panel4.add(panel7, java.awt.BorderLayout.CENTER);
 
         panel2.add(panel4);
 
-        javax.swing.GroupLayout panel5Layout = new javax.swing.GroupLayout(panel5);
-        panel5.setLayout(panel5Layout);
-        panel5Layout.setHorizontalGroup(
-            panel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 458, Short.MAX_VALUE)
-        );
-        panel5Layout.setVerticalGroup(
-            panel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 56, Short.MAX_VALUE)
-        );
+        panel5.setLayout(new java.awt.GridLayout(1, 0));
+
+        jButton4.setBackground(new java.awt.Color(163, 144, 129));
+        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Archivos/Tienda4.png"))); // NOI18N
+        panel5.add(jButton4);
+
+        jButton5.setBackground(new java.awt.Color(163, 144, 129));
+        jButton5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vista/Novedades.png"))); // NOI18N
+        panel5.add(jButton5);
 
         panel2.add(panel5);
 
-        jLabel5.setText("jLabel4");
-        panel2.add(jLabel5);
+        panel6.setLayout(new java.awt.GridLayout(1, 0));
+
+        jLabel2.setFont(new java.awt.Font("Poppins", 0, 36)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Contactanos: contacto@miumiu.co");
+        jLabel2.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        panel6.add(jLabel2);
+
+        panel2.add(panel6);
 
         panel1.add(panel2, java.awt.BorderLayout.CENTER);
 
@@ -148,6 +156,14 @@ public class PaginaPrincipal extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -185,11 +201,15 @@ public class PaginaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
@@ -202,5 +222,6 @@ public class PaginaPrincipal extends javax.swing.JFrame {
     private java.awt.Panel panel4;
     private java.awt.Panel panel5;
     private java.awt.Panel panel6;
+    private java.awt.Panel panel7;
     // End of variables declaration//GEN-END:variables
 }
